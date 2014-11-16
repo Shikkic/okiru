03db1a06a4ca1d876c7589d86d5e8abf2c61c891
Grandclosing <vukpetrovic1@yahoo.com>
2014-11-16 10:35:35 -0500

    Completed JSON data formatting and Jquery transfer via AJAX

---
+++ public/test.php
@@ -0,0 +1,12 @@
+
+<?php
+	$phone = $_POST['phone'];
+	$sun = $_POST['sun'];
+	$mon = $_POST['mon'];
+	$tue = $_POST['tue'];
+	$wed = $_POST['wed'];
+	$thu = $_POST['thu'];
+	$fri = $_POST['fri'];
+	$sat = $_POST['sat'];
+	echo "\$phone: $phone, \$sun: $sun, \$mon: $mon, \$tue: $tue, \$wed: $wed, \$thu: $thu, \$fri: $fri, \$sat: $sat";
+?>
