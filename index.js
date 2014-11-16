bb0d305957729fa471a8d9955c6dba0e0883762a
Ubuntu <dan@natsuko.okiru.f5.internal.cloudapp.net>
2014-11-16 02:07:56 +0000

    begining passport implementation

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 var express = require('express');
-
 var app = express();
-
 app.use(express.static('public'));
 
+var dataBase = require('./db.js');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/mydb');
-var Schema = mongoose.Schema;
+moongose.connect(dataBase.url);
 
-var AlarmSchema = new Schema({
-  name:  String,
-  phone: String,
-  venmoId: Number,
-  date: { type: Date, default: Date.now },
+module.exports = mongoose.model('User',{
+    venmoId: String,
+	phone: String
+});
+
+var passport = require('passport');
+var expressSession = require('express-session');
+app.use(expressSession({secret: 'mySecretKey'}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done){
+	done(null,user._id);
+});
+
+passport.deserializeUser(function(user, done){
+	User.findById(id,function(err, user){
+		done(err, user);
+	});
 });
 
 app.get('/', function(req, res){
@@ -23,3 +35,4 @@ app.listen(80, function(){
 	console.log('Serve is listening on port 80');
 });
 
+	
