ce2701bc31d2491fdf951e705f7e7c4c361e8a97
Ubuntu <dan@natsuko.okiru.f5.internal.cloudapp.net>
2014-11-16 01:03:36 +0000

    moongose update

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ var app = express();
 
 app.use(express.static('public'));
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/mydb');
+var Schema = mongoose.Schema;
+
+var AlarmSchema = new Schema({
+  name:  String,
+  phone: String,
+  venmoId: Number,
+  date: { type: Date, default: Date.now },
+});
+
 app.get('/', function(req, res){
 	res.sendfile('public/index.html');
 });
 
 app.listen(80, function(){
-	console.log('Serve is listening');
+	console.log('Serve is listening on port 80');
 });
 
